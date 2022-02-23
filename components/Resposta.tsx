e41a7134cac6dc31cce9271bc932a35dd38a63d5
vilmarmoraes <vilmarpomifrai@hotmail.com>
2022-02-23 13:15:25 -0300

    Estilizado componente resposta

--- components/Resposta.tsx
+++ components/Resposta.tsx
@@ -5,7 +5,7 @@ interface RespostaProps{
     valor: RespostaModel
     indice: number
     letra: string
-    corLetra: string
+    corFundoLetra: string
 }
 
 export default function Resposta(props: RespostaProps) {
@@ -13,17 +13,27 @@ export default function Resposta(props: RespostaProps) {
 
     return (
         <div className={styles.resposta}> 
+            
             <div className={styles.conteudoResposta}>
+                
                 <div className={styles.frente}>
-                    <div className={styles.letra}>
+                    
+                    <div className={styles.letra}
+                        style={{ backgroundColor: props.corFundoLetra}}>                        
                         {props.letra}
                     </div>
+                    
                     <div className={styles.valor}>
                         {resposta.valor}
-                    </div>                    
+                    </div>  
+
                 </div>
+
+
                 <div className={styles.verso}>
+
                 </div>
+
             </div>        
 
         </div>
